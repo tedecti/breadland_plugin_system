000b5839188da7752029c8022d16ff87af586409
tedecti <wolftheguy08@gmail.com>
2025-05-18 18:56:46 +0500

    initial 1.0

---
+++ src/main/java/me/tedecti/breadland/Breadland.java
@@ -0,0 +1,17 @@
+package me.tedecti.breadland;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Breadland extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
